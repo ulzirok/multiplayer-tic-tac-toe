ede908848022f2cac5d128ca1d073e74f0709235
ulzirok <ulzirok@gmail.com>
2026-02-13 02:28:53 +0500

    add client url to the cors config

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const { handleClose } = require('./socket/socketController')
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-app.use(cors({origin: ['http://localhost:5173', 'https://frontend.com'] }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://cozy-douhua-c88ba3.netlify.app'] }));
 
 app.use('/api', router);
 
