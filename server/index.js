7027fdf59fbf7e731a0e17a878b3912b378c6d59
ulzirok <ulzirok@gmail.com>
2026-02-13 02:23:43 +0500

    add baseUrl

--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ const { handleClose } = require('./socket/socketController')
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://frontend.com'] }));
+app.use(cors({origin: ['http://localhost:5173', 'https://frontend.com'] }));
 
 app.use('/api', router);
 
